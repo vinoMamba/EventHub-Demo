bc000e12083e588ea33f37eeb4065b00f6e769e0
vino <vino0908@outlook.com>
2021-03-06 17:57:33 +0800

    第三版：添加event.off

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,25 @@ class EventHub {
         this.cache[eventName] = this.cache[eventName] || []
         this.cache[eventName].push(fn)
     }
-    emit(eventName, data) {
+    emit(eventName, data?) {
         (this.cache[eventName] || []).forEach(fn => fn(data));
     }
+    off(eventName, fn) {
+        const index = indexOf(this.cache[eventName], fn)
+        if (index === -1) return
+        this.cache[eventName].splice(index, 1)
+    }
 }
-
 export default EventHub
+
+function indexOf(array, item) {
+    if (array === undefined) return -1
+    let index = -1
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            index = i
+            break
+        }
+    }
+    return index
+}
--- test/test.ts
+++ test/test.ts
@@ -14,3 +14,16 @@ eventHub.on('xxx', (data) => {
 
 //发布
 eventHub.emit('xxx', 'xxx事件的内容')
+
+const eventHub2 = new EventHub()
+let called2 = false
+const fn1 = () => {
+    called2 = true
+}
+eventHub.on('yyy', fn1)
+eventHub.off('yyy', fn1)
+eventHub.emit('yyy')
+
+setTimeout(() => {
+    console.log(called2)
+}, 2000);
